f7d8a2b350c35f4e609a742a0c6c7320e5c43297
Matt Snider <matt.snider@alum.utoronto.ca>
2017-08-20 20:51:49 +0200

    Return [] from db when no notes are present

--- src/db/api/tasks.sql
+++ src/db/api/tasks.sql
@@ -2,9 +2,11 @@
 CREATE VIEW tasks AS
     SELECT
         t.*,
-        json_agg(json_build_object('id', n.id, 'content', n.content))
-            FILTER (WHERE n.id IS NOT NULL)
-            AS notes
+        coalesce(
+            json_agg(json_build_object('id', n.id, 'content', n.content))
+                FILTER (WHERE n.id IS NOT NULL),
+            json_build_array()
+        ) AS notes
     FROM models.tasks t LEFT JOIN models.notes n
         ON t.id = n.task_id
     GROUP BY t.id;
