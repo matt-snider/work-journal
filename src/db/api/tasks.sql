9534be661728cc05d2ced270c257695f15f93057
Matt Snider <matt.snider@alum.utoronto.ca>
2017-08-20 20:52:19 +0200

    Implement insertion on tasks view (not incl. of notes)

--- src/db/api/tasks.sql
+++ src/db/api/tasks.sql
@@ -10,3 +10,29 @@ CREATE VIEW tasks AS
     FROM models.tasks t LEFT JOIN models.notes n
         ON t.id = n.task_id
     GROUP BY t.id;
+
+
+CREATE OR REPLACE FUNCTION api.insert_task()
+    RETURNS trigger AS $$
+    DECLARE
+        task_id bigint;
+        result record;
+    BEGIN
+        -- Insert task record
+        INSERT INTO models.tasks (description, is_complete)
+        VALUES (new.description, false)
+        RETURNING id INTO task_id;
+
+        -- Use our view to return the result
+        -- including new id and notes
+        SELECT * INTO result FROM api.tasks WHERE id = task_id;
+        RETURN result;
+    END;
+    $$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER tasks_update
+    INSTEAD OF INSERT
+    ON tasks
+    FOR EACH ROW
+    EXECUTE PROCEDURE insert_task();
