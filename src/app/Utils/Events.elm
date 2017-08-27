dd755786b194a0189f2bc9ad53cba2421b98e625
Matt Snider <matt.snider@alum.utoronto.ca>
2017-08-27 16:14:00 +0200

    Add onEnter Attribute (without Input contents)

--- src/app/Utils/Events.elm
+++ src/app/Utils/Events.elm
@@ -3,12 +3,12 @@ module Utils.Events exposing (..)
 import Debug
 import Html exposing (..)
 import Html.Events exposing (..)
-import Json.Decode as Decode
+import Json.Decode as Json
 
 
 onInputBlur : (String -> msg) -> Attribute msg
 onInputBlur tagger =
-    on "blur" (Decode.map tagger targetValue)
+    on "blur" (Json.map tagger targetValue)
 
 
 onInputEnter : (String -> msg) -> Attribute msg
@@ -16,13 +16,25 @@ onInputEnter tagger =
     let
         isEnter code =
             if code == 13 then
-                Decode.succeed ""
+                Json.succeed ""
             else
-                Decode.fail ""
+                Json.fail ""
         decodeEnter =
-            Decode.andThen isEnter keyCode
+            Json.andThen isEnter keyCode
     in
         on "keydown"
-        <| Decode.map2 (\_ value -> Debug.log "tagger value" (tagger value))
+        <| Json.map2 (\_ value -> Debug.log "tagger value" (tagger value))
             decodeEnter
             targetValue
+
+
+onEnter : msg -> Attribute msg
+onEnter message =
+    let
+        isEnter code =
+            if code == 13 then
+                Json.succeed message
+            else
+                Json.fail "Not enter"
+    in
+        on "keydown" (Json.andThen isEnter keyCode)
