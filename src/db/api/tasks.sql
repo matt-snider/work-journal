69934cfecd0cff68f507fe09ebe30f7e65f73015
Matt Snider <matt.snider@alum.utoronto.ca>
2017-09-02 02:29:10 +0200

    Return new id from db when creating new tasks

--- src/db/api/tasks.sql
+++ src/db/api/tasks.sql
@@ -8,6 +8,7 @@ CREATE FUNCTION save_task()
     RETURNS trigger AS $$
     DECLARE
         is_notes_valid BOOLEAN;
+        new_id INT;
         result RECORD;
     BEGIN
         -- Ensure notes is just a simple array
@@ -29,12 +30,15 @@ CREATE FUNCTION save_task()
                SET description = new.description,
                    is_complete = new.is_complete,
                    notes = new.notes
-            WHERE  id = new.id;
+            WHERE id = new.id;
+            new_id = new.id;
         ELSE
             INSERT INTO models.tasks (description, is_complete, notes)
-                VALUES (new.description, new.is_complete, new.notes);
+                VALUES (new.description, new.is_complete, new.notes)
+                RETURNING id INTO new_id;
         END IF;
-        RETURN new;
+        SELECT * INTO result FROM api.tasks WHERE id = new_id;
+        RETURN result;
     END;
     $$ LANGUAGE plpgsql;
 
